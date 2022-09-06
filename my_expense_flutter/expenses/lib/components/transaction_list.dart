1327a4750afa9c05823ef4db4a937ff8546dbb87
QuatrochiFh <quatrochifabio145@gmail.com>
2022-09-06 18:06:03 -0300

    responsividade

--- my_expense_flutter/expenses/lib/components/transaction_list.dart
+++ my_expense_flutter/expenses/lib/components/transaction_list.dart
@@ -12,87 +12,88 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 440,
-      child: transactions.isEmpty
-          ? Column(
-              children: [
-                const SizedBox(height: 20),
-                Text(
-                  'Nenhuma Transação Cadastrada!',
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-                const SizedBox(height: 20),
-                SizedBox(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
+    return transactions.isEmpty
+        ? LayoutBuilder(
+            builder: (ctx, Constraints) {
+              return Column(
+                children: [
+                 const SizedBox(height: 20),
+                  Text(
+                    'Nenhuma Transação Cadastrada!',
+                    style: Theme.of(context).textTheme.headline6,
                   ),
-                ),
-              ],
-            )
-          : ListView.builder(
-              itemCount: transactions.length,
-              itemBuilder: (ctx, index) {
-                final tr = transactions[index];
-                return Card(
-                  //elevation colocará os cards,
-                  //separando cada despesa
-                  elevation: 5,
-                  margin: const EdgeInsets.symmetric(
-                    //aqi temos a margin dos cards
-                    // tanto usada na vertical 
-                    //quanto na horizontal
-                    // |
-                    vertical: 10,
-                    // --
-                    horizontal: 24,
+                  const SizedBox( height: 20 ),
+                  SizedBox(
+                    height: Constraints.maxHeight * 0.5,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                  child: ListTile(
-                    //ira virar um circulo
-                    //como um circulo de avatar
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.green,
-                     //tamanho do 'raio'circulo
-                      radius: 30,
-                      child: Padding(
-                        //tamanho do texto de dentro
-                        padding: const EdgeInsets.all(3),
-                        //aqui o FittedBox impede que seus widgets
-                        //filhos aumentem de tamanho além de um 
-                        //determinado limite
-                        child: FittedBox(
-                          child: Text(
-                            'R\$${tr.value}',
-                            style: const TextStyle(
-                              //cor do texto de dentro
-                              color: Colors.white,
-                            ),
+                ],
+              );
+            },
+          )
+        : ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (ctx, index) {
+              final tr = transactions[index];
+              return Card(
+                //elevation colocará os cards,
+                //separando cada despesa
+                elevation: 5,
+                margin: const EdgeInsets.symmetric(
+                  //aqi temos a margin dos cards
+                  // tanto usada na vertical
+                  //quanto na horizontal
+                  // |
+                  vertical: 10,
+                  // --
+                  horizontal: 24,
+                ),
+                child: ListTile(
+                  //ira virar um circulo
+                  //como um circulo de avatar
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.green,
+                    //tamanho do 'raio'circulo
+                    radius: 30,
+                    child: Padding(
+                      //tamanho do texto de dentro
+                      padding: const EdgeInsets.all(3),
+                      //aqui o FittedBox impede que seus widgets
+                      //filhos aumentem de tamanho além de um
+                      //determinado limite
+                      child: FittedBox(
+                        child: Text(
+                          'R\$${tr.value}',
+                          style: const TextStyle(
+                            //cor do texto de dentro
+                            color: Colors.white,
                           ),
                         ),
                       ),
                     ),
-                    //texto do lado do avatar de dinheiro 
-                    //com o titulo
-                    title: Text(
-                      tr.title,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    //data do lado do avatar de dinheiro 
-                    //e baixo do titulo
-                    subtitle: Text(
-                      DateFormat('d MMM y').format(tr.date),
-                    ),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () => onRemove(tr.id),
-                      ),
                   ),
-                );
-              },
-            ),
-    );
+                  //texto do lado do avatar de dinheiro
+                  //com o titulo
+                  title: Text(
+                    tr.title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  //data do lado do avatar de dinheiro
+                  //e baixo do titulo
+                  subtitle: Text(
+                    DateFormat('d MMM y').format(tr.date),
+                  ),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                    onPressed: () => onRemove(tr.id),
+                  ),
+                ),
+              );
+            },
+          );
   }
 }
--- my_expense_flutter/expenses/lib/main.dart
+++ my_expense_flutter/expenses/lib/main.dart
@@ -3,6 +3,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import './models/transaction.dart';
 //import './components/transaction_user.dart';
 import './components/transaction_form.dart';
@@ -18,49 +19,46 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // o app não girar na horizontal, mas não vamos usar isso, só de exemplo
+    //SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
     //Aqui estamos criando uma variável que irá armazenas o nosso tema
     final ThemeData tema = ThemeData();
 
-
-    return  MaterialApp(
+    return MaterialApp(
       home: const MyHomePage(),
       //aqui iremos referenciar o theme do materialApp
       //o copy.With Este método pega todas as propriedades (que precisam ser alteradas)
       // e seus valores correspondentes e retorna um novo objeto com as propriedades desejadas.
       theme: tema.copyWith(
-        //o colorScheme é o esquema de cores, ou seja ele ira fazer o esquema de cores desejado
-        colorScheme: tema.colorScheme.copyWith(
-          //cor primaria
-          primary: Colors.green,
-          //cor secundaria 
-          secondary: Colors.green[800],
-          
-        ),
-        //tema do texto
-        textTheme: tema.textTheme.copyWith(
-          headline6: const TextStyle(
-            fontFamily: 'OpenSans',
-            fontSize:15,
-            fontWeight: FontWeight.bold,
-            color: Colors.black
+          //o colorScheme é o esquema de cores, ou seja ele ira fazer o esquema de cores desejado
+          colorScheme: tema.colorScheme.copyWith(
+            //cor primaria
+            primary: Colors.green,
+            //cor secundaria
+            secondary: Colors.green[800],
           ),
-          button: const TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          )
-        ),
-        appBarTheme: const AppBarTheme(
-          titleTextStyle:  TextStyle(
+          //tema do texto
+          textTheme: tema.textTheme.copyWith(
+              headline6: const TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+              button: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              )),
+          appBarTheme: const AppBarTheme(
+              titleTextStyle: TextStyle(
             fontFamily: 'Quicksand',
             fontSize: 30,
             fontWeight: FontWeight.bold,
-          )
-        )
-      ) ,
+          ))),
     );
   }
 }
- 
+
 //
 
 class MyHomePage extends StatefulWidget {
@@ -71,15 +69,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Transaction>_transactions = [];
+  final List<Transaction> _transactions = [];
+  bool _showChart = false;
 
   List<Transaction> get _recentTransactions {
-    return _transactions.where((tr){
+    return _transactions.where((tr) {
       //se a data for menor que uma data de 7 dias atras é verdadeiro
       return tr.date.isAfter(DateTime.now().subtract(
         const Duration(days: 7),
       ));
-    }).toList( );
+    }).toList();
   }
 
   _addTransaction(String title, double value, DateTime date) {
@@ -97,8 +96,9 @@ class _MyHomePageState extends State<MyHomePage> {
     //aqui ele esta escondendo o modal, logo apos o envio da transação
     Navigator.of(context).pop();
   }
+
   //Aqui vamos deletar a despesa
-  _removeTransaction(String id){
+  _removeTransaction(String id) {
     setState(() {
       //o removeWhere servira de filtro para remover a lista de acordo com oq passarmos
       _transactions.removeWhere((tr) {
@@ -108,8 +108,8 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-    //aqui abriremos o modal 
-   _openTransactionFormModal(BuildContext context) {
+  //aqui abriremos o modal
+  _openTransactionFormModal(BuildContext context) {
     //adcionaremos a conta
     showModalBottomSheet(
       context: context,
@@ -119,26 +119,74 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Despesas Pessoais'),
-        actions: <Widget>[
-          IconButton(
-              onPressed: () => _openTransactionFormModal(context),
-              icon: const Icon(Icons.add_circle_outline))
-        ],
+    //so iremos exibir o grafico se ele estiver no modo paisagem
+   bool isLandscape = MediaQuery.of(context).orientation ==  Orientation.landscape;
+    //appBar de baixo veio pra cá para alteramos coisas
+    final appBar = AppBar(
+      title: Text(
+        'Despesas Pessoais',
+        //vamos fazer com que a fonte possa ser mudada de acordo com
+        //o que o usuario quiser e/ou tamanho do celular
+        //isso é muito importante para acessibilidade
+        style: TextStyle(
+          fontSize: 20 * MediaQuery.of(context).textScaleFactor,
+        ),
       ),
+      actions: <Widget>[
+        IconButton(
+            onPressed: () => _openTransactionFormModal(context),
+            icon: const Icon(Icons.add_circle_outline))
+      ],
+    );
+    //Aqui estartemos fazendo o responsivo, colocando o mediaQuery, subistuindo
+    // um heigth unico; como heigth: 400
+    final availableHeigth = MediaQuery.of(context).size.height -
+        //com isso tiraremos o scrool da aplicação e so deixaremos o scrool para lista
+        appBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
+    return Scaffold(
+      //o appbar que estava aqui, foi transferido para cima,
+      //para podermos fazer modificações nas resposividades
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment
               .stretch, // Serve para colocar as coisas no centro/Começo/Final ou como nesse caso, esta esticando
           children: <Widget>[
-           Chart(_recentTransactions),  
-            TransactionList(_transactions, _removeTransaction),
+            //exibir grafico
+            if(isLandscape)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('Exibir o Grafico?'),
+                //habilita o visual de ligar/desligar
+                Switch(
+                  value: _showChart,
+                  onChanged: (value) {
+                    setState(() {
+                      _showChart = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+            //se o _showChart for verdadeiro exibe esse Sizebox primeiro
+            if (_showChart || !isLandscape)
+              SizedBox(
+                  //aqui no chart da aplicação ele vai pegar o heigth com 30% da tela.
+                  //por isso o multiplicado por 3
+                  height: availableHeigth * (isLandscape ? 0.7 : 0.3),
+                  child: Chart(_recentTransactions)),
+            //se for falso exibe esse
+            if (!_showChart || !isLandscape )
+              SizedBox(
+                  //aqui no TransactionList da aplicação ele vai pegar o heigth com 60% da tela.
+                  //por isso o multiplicado por 6
+                  height: availableHeigth * 0.7,
+                  child: TransactionList(_transactions, _removeTransaction)),
           ],
         ),
       ),
